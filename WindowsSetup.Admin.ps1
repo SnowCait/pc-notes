0cc6b7302439ee3542d1fcee15cf11856107fe71
雪猫 <SnowCait@users.noreply.github.com>
2019-08-26 01:32:20 +0900

    Update WindowsSetup.Admin.ps1

--- WindowsSetup.Admin.ps1
+++ WindowsSetup.Admin.ps1
@@ -1,1 +1,5 @@
+# Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# jq
+chocolatey install jq
