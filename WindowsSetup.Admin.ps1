2b1d18c215fef8c95e1cf06f40b4bdcf5a24051d
雪猫 <SnowCait@users.noreply.github.com>
2019-12-03 00:41:38 +0900

    Update WindowsSetup.Admin.ps1

--- WindowsSetup.Admin.ps1
+++ WindowsSetup.Admin.ps1
@@ -1,3 +1,6 @@
+# Usage
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/SnowCait/pc-notes/master/WindowsSetup.Admin.ps1'))
+
 # Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
