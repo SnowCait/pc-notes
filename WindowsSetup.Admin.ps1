5a3249ff8b8bd3592ceba306dca97870114d4f79
雪猫 <SnowCait@users.noreply.github.com>
2022-02-06 02:24:25 +0900

    Update WindowsSetup.Admin.ps1

--- WindowsSetup.Admin.ps1
+++ WindowsSetup.Admin.ps1
@@ -5,5 +5,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y googlechrome
-choco install -y fiddler
+#choco install -y fiddler
 choco install -y jq
